51cd89db9fb1f2171120870d037596c752cc71cc
Sacha Best <sachabest@outlook.com>
2014-06-22 21:05:52 -0700

    Merged MainView and ViewConroller to same scene Added navigation bar with bar button item for logout The old logout button was covered by the swipe to choose view so taps would not register

--- TinderLink/MainViewController.m
+++ TinderLink/MainViewController.m
@@ -46,6 +46,48 @@
     view.imageView.image = [UIImage imageNamed:@"photo"];
     [self.view addSubview:view];
 }
+- (void)presentLogin {
+    // Create the log in view controller
+    PFLogInViewController *logInViewController = [[PFLogInViewController alloc] init];
+    logInViewController.delegate = self;
+    
+    // Create the sign up view controller
+    PFSignUpViewController *signUpViewController = [[PFSignUpViewController alloc] init];
+    signUpViewController.delegate = self;// Set ourselves as the delegate
+    
+    LIALinkedInApplication *application = [LIALinkedInApplication applicationWithRedirectURL:@"http://www.ancientprogramming.com/liaexample"
+                                                                                    clientId:@"75t78lkjc036lt"
+                                                                                clientSecret:@"dl4C98WcMNyMDSb4"
+                                                                                       state:@"DCEEFWF45453sdffef424"
+                                                                               grantedAccess:@[@"r_fullprofile", @"r_emailaddress",@" r_contactinfo"]];
+    
+    [LIALinkedInHttpClient clientForApplication:application presentingViewController:nil];
+    
+    // Assign our sign up controller to be displayed from the login controller
+    [logInViewController setSignUpController:signUpViewController];
+    
+    // Present the log in view controller
+    [self presentViewController:logInViewController animated:NO completion:NULL];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    if (![PFUser currentUser]) { // No user logged in
+        [self presentLogin];
+    }
+}
+
+- (void)signUpViewController:(PFSignUpViewController *)signUpController didSignUpUser:(PFUser *)user {
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+}
+
+- (void)logInViewController:(PFLogInViewController *)logInController didLogInUser:(PFUser *)user {
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 
 // This is called when a user didn't fully swipe left or right.
 - (void)viewDidCancelSwipe:(UIView *)view {
@@ -79,10 +121,10 @@
 }
 */
 
-- (IBAction)logOutPressed {
-    
+- (IBAction)logOut:(id)sender {
+    NSLog(@"fired");
     [PFUser logOut];
-    [self performSegueWithIdentifier:@"toLogIn" sender:self];
+    [self presentLogin];
     
     
 }
